0a5e4c9c747399412ec74844483302c41d9b8510
dome4 <dominic.seitz@gmx.de>
2018-02-22 15:44:56 +0100

    dump data

--- tree.py
+++ tree.py
@@ -8,26 +8,24 @@ def createTree():
 
     """
     Die Reihenfolge spielt eine wichtige Rolle !!!
+    Files können mehr als einen Parent haben in Google!
+    Beispiel-Baum
     """
-    node7 = NodeClass('Ordner1', 12345)
-    node6 = NodeClass('Ordner2', 12345, parent = node7)
-    node5 = NodeClass('Ordner3', 12345, parent = node7)
+    root = NodeClass('root')
     
-    node0 = NodeClass('Dok1', 12345, parent = node7)
-    node1 = NodeClass('Dok2', 12345, parent = node6)
-    node2 = NodeClass('Dok3', 12345, parent = node5)
-    node3 = NodeClass('Dok4', 12345, parent = node5)
-    node4 = NodeClass('Dok5', 12345)
-    
-    
-    """
-    Beispiel-Baum aufbauen
-    """
+    node1 = NodeClass(name = 'API Test', id = '1cGR1jyzi6irzPRsl85Daz_MY4GTr9Xs_', fileType = 'application/vnd.google-apps.folder', parent = root)
+    node2 = NodeClass(name = 'Folder 1', id = '1lWx0ibKWx7ZEfBlquFvgM28DCcOLDIzJ', fileType = 'application/vnd.google-apps.folder', parent = node1)
+    node3 = NodeClass(name = 'Folder 2', id = '1kJtVfm26RzHi7cNKuIhlh5tvtV-Ezjf8', fileType = 'application/vnd.google-apps.folder', parent = node1)    
+    node4 = NodeClass(name = 'Folder 1_1', id = '1yycs6WSYGIAB0yZqIJ1vSTzqoBSYNJw-', fileType = 'application/vnd.google-apps.folder', parent = node2)    
+    node5 = NodeClass(name = 'Folder 1_2', id = '1pR_TJYMHi15dGSJbzFPNMoDkUcNX3pZ6', fileType = 'application/vnd.google-apps.folder', parent = node2)    
+    node6 = NodeClass(name = 'Test', id = '1lWYBhzUWX5wj-OjRKWfW5YCWr6LJWV_IN0tPTgirluc', fileType = 'application/vnd.google-apps.document', parent = node4)
+    node7 = NodeClass(name = 'Test 2', id = '11seIce8iS3NvP7Nr8qAa79xmTkDMD9hjmp3yakYG3NY', fileType = 'application/vnd.google-apps.spreadsheet', parent = node5)
+
     
     """
     render tree
     """
-    for pre, _, node in RenderTree(node7): #root component
+    for pre, _, node in RenderTree(root): #root component
         print("%s%s" % (pre, node.name))
     
 
@@ -35,7 +33,7 @@ def createTree():
 class of the tree nodes 
 """
 class NodeClass(NodeMixin):
-    def __init__(self, name, id, fileType, parent=None):
+    def __init__(self, name, id=None, fileType=None, parent=None):
         super(NodeClass, self).__init__()
         self.name = name
         self.id = id
