eba25bf67bf87535d714e00c317b7263037f1186
dome4 <dominic.seitz@gmx.de>
2018-02-26 22:13:14 +0100

    downloadFile-method

--- createFileStructure.py
+++ createFileStructure.py
@@ -11,3 +11,40 @@ def createFolder(directory):
 # Example
 # createFolder('./data/')
 # Creates a folder in the current directory called data
+
+def downloadFile(service, fileID, fileName, mimeType, targetPath):
+    
+    """
+    Download Google Files
+    """
+    if 'application/vnd.google-apps' in mimeType:
+            if 'form' in mimeType:
+                print('Google Form - cannot be downloaded. Skiping...' + str(fileID))
+                
+            elif 'document' in mimeType:
+                request = service.files().export_media(fileId=fileID, mimeType='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+                fileName = fileName + '.docx'
+                
+            elif 'spreadsheet' in mimeType:
+                request = service.files().export_media(fileId=fileID, mimeType='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+                fileName = fileName + '.xlsx' 
+                   
+            elif 'presentation' in mimeType:
+                request = service.files().export_media(fileId=fileID, mimeType='application/vnd.openxmlformats-officedocument.presentationml.presentation')
+                fileName = fileName + '.pptx'
+            else:
+                print ('unhandeld format: ' + str(mimeType))
+                    
+    else: 
+        request = service.files().drive_service.files().get_media(fileId=fileID)
+      
+    """
+    Download other files
+    """ 
+                           
+    print("Downloading -- {}".format(fileName))
+    
+    response = request.execute()
+    
+    with open(os.path.join(targetPath, fileName), "wb") as wer:
+        wer.write(response)
