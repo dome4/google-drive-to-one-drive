11938395cc9dd4ba1711d50f6161e7714987d52f
dome4 <dominic.seitz@gmx.de>
2018-03-03 21:31:05 +0100

    created config file

---
+++ config.default.py
@@ -0,0 +1,23 @@
+# 
+# configuration file
+#
+"""
+folder in which the downloaded content is stored
+"""
+DOWNLOAD_FOLDER='./data/'
+
+# enable / disable different parts of the script
+"""
+download metadata from google drive
+"""
+DOWNLOAD_METADATA=True
+
+"""
+create the tree of the downloaded metadata
+"""
+CREATE_TREE=True
+
+"""
+create the file structure and download the files
+"""
+CREATE_STRUCTURE=True
